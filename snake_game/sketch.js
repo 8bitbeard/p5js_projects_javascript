0de50f33e3f43f91b89c9b835891f2050e38497a
8bitbeard <wosf@cin.ufpe.br>
2019-09-12 23:47:11 -0300

    Initial commit for snake_game

---
+++ snake_game/sketch.js
@@ -0,0 +1,53 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+	createCanvas(400, 400);
+	w = floor(width / rez);
+	h = floor(height / rez);
+	frameRate(10);
+	snake = new Snake()
+	foodLocation();
+}
+
+function foodLocation() {
+	let x = floor(random(w));
+	let y = floor(random(h));
+	food = createVector(x, y);
+}
+
+function keyPressed() {
+	if (keyCode == LEFT_ARROW) {
+		snake.setDir(-1, 0);
+	}
+	else if (keyCode == RIGHT_ARROW) {
+		snake.setDir(1, 0);
+	}
+	else if (keyCode == DOWN_ARROW) {
+		snake.setDir(0, 1);
+	}
+	else if (keyCode == UP_ARROW) {
+		snake.setDir(0, -1);
+	}
+}
+
+function draw() {
+	scale(rez);
+	background(220);
+	if (snake.eat(food)) {
+		foodLocation();
+	}
+	snake.update();
+	snake.show();
+	if (snake.endGame()) {
+		print("END GAME");
+		background(255, 0, 0);
+		noLoop();
+	}
+	noStroke();
+	FileList(255, 0, 0);
+	rect(food.x, food.y, 1, 1);
+}
