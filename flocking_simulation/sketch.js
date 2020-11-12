97ef5bb0c3b4c65fed937ab9ef10af121037f229
Wilton Souza <willsouzafilho@gmail.com>
2020-11-12 08:46:15 -0300

    Adding the flocking simulation project

---
+++ flocking_simulation/sketch.js
@@ -0,0 +1,24 @@
+const boids = [];
+
+let alignSlider, cohesionSlider, separationSlider;
+
+function setup() {
+  createCanvas(640, 360);
+  alignmentSlider = createSlider(0, 5, 1, 0.1);
+  cohesionSlider = createSlider(0, 5, 1, 0.1);
+  separationSlider = createSlider(0, 5, 1, 0.1);
+  for (var i = 0; i < 100; i++) {
+    boids.push(new Boid());
+  }
+}
+
+function draw() {
+  background(51);
+
+  for(let boid of boids) {
+    boid.edges();
+    boid.flock(boids);
+    boid.update();
+    boid.show();
+  }
+}
