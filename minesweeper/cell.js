327e9bf774934d9095acdd29c61335eb43acefcb
Wilton Souza <willsouzafilho@gmail.com>
2020-11-14 09:45:28 -0300

    Adding the Minesweeper Project

---
+++ minesweeper/cell.js
@@ -0,0 +1,81 @@
+class Cell {
+
+  constructor(i, j, w) {
+    this.i = i;
+    this.j = j;
+    this.x = i * w;
+    this.y = j * w;
+    this.w = w;
+    this.neighborCount = 0;
+
+    this.bee = false;
+    this.revealed = false;
+  }
+
+  countBees() {
+    if (this.bee) {
+      this.neighborCount = -1;
+      return;
+    }
+    var total = 0;
+    for (var xoff = -1; xoff <= 1; xoff++) {
+      for (var yoff = -1; yoff <= 1; yoff++) {
+        var i = this.i + xoff;
+        var j = this.j + yoff;
+        if (i > -1 && i < cols && j > -1 && j < rows) {
+          var neighbor = grid[i][j];
+          if (neighbor.bee) {
+            total++;
+          }
+        }
+      }
+    }
+    this.neighborCount = total;
+  }
+
+  contains(x, y) {
+    return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
+  }
+
+  reveal() {
+    this.revealed = true;
+    if (this.neighborCount == 0) {
+      this.floodFill();
+    }
+  }
+
+  floodFill() {
+    for (var xoff = -1; xoff <= 1; xoff++) {
+      for (var yoff = -1; yoff <= 1; yoff++) {
+        var i = this.i + xoff;
+        var j = this.j + yoff;
+        if (i > -1 && i < cols && j > -1 && j < rows) {
+          var neighbor = grid[i][j];
+          if (!neighbor.bee && !neighbor.revealed) {
+            neighbor.reveal();
+          }
+        }
+      }
+    }
+  }
+
+  show() {
+    stroke(0);
+    noFill();
+    rect(this.x, this.y, this.w, this.w)
+    if (this.revealed) {
+      if (this.bee) {
+        fill(127);
+        ellipse(this.x + this.w * 0.5 , this.y + this.w * 0.5, this.w * 0.5)
+      } else {
+        fill(200);
+        rect(this.x, this.y, this.w, this.w)
+        if (this.neighborCount > 0) {
+          textAlign(CENTER);
+          fill(0);
+          text(this.neighborCount, this.x + this.w  * 0.5, this.y + this.w - 5);
+        }
+      }
+    }
+  }
+}
---
+++ minesweeper/sketch.js
@@ -0,0 +1,81 @@
+function make2DArray(cols, rows) {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+var grid;
+var cols = 20;
+var rows = 20;
+w = 20;
+
+var totalBees = 15;
+
+function setup() {
+  createCanvas(201, 201);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = make2DArray(cols, rows);
+  console.log(grid);
+  for (var i = 0; i < cols; i ++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j]  new Cell(i, j, w);
+    }
+  }
+
+  var options = [];
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++){
+      options.push([i, j]);
+    }
+  }
+
+  for (var n = 0; n < totalBees; n++) {
+    var index = floor(random(options.length));
+    var choice = options[index]
+    var i = choice[0];
+    var j = choice[1];
+    options.splice(index, 1);
+    grid[i][j].bee = true;
+  }
+
+
+  for (var i = 0; i < cols; i ++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].countBees();
+    }
+  }
+}
+
+function gameOver() {
+  for (var i = 0; i < cols; i ++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].revealed = true;
+    }
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < cols; i ++) {
+    for (var j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY)) {
+        grid[i][j].reveal();
+
+        if(grid[i][j].bee) {
+          gameOver();
+        }
+      }
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  for (var i = 0; i < cols; i ++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
+}
